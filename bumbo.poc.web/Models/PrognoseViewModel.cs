a77f7fcd01b88e32d9165e6f46822253dd4b577c
Picklemaniac <bm.lemmen2@student.avans.nl>
2022-10-15 15:00:40 +0200

    Created basic create view, added basic create option, no data transfer yet

---
+++ bumbo.poc.web/Models/PrognoseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using bumbo.poc.data.Models;
+using bumbo.poc.data.Repositories;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Xml.Linq;
+
+namespace bumbo.poc.web.Models
+{
+    public class PrognoseViewModel
+    {
+        public int BranchId { get; set; }
+
+        public int year { get; set;}
+
+        public int week { get; set; }
+
+        //Relatief? Beetje beun
+        public int DaysInWeek = 7;
+        public DateTime FirstDayOfWeek { get; set; }
+        public List<int> ExpectedPackages { get; set; }
+        public List<int> ExpectedCustomers { get; set; }
+    }
+}
